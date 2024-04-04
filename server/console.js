94ed3a1f814625a91fc876e9822c0935fdd11605
Enzoo_Kms <ekemmas@edenschool.fr>
2024-04-04 16:32:28 +0200

    change console loader

--- server/console.js
+++ server/console.js
@@ -1,6 +1,78 @@
+const fs = require("fs");
 require("colors");
-function load() {
+
+const loading = async (path) => {
+  const date = new Date();
+
+  // Extraction des composants de la date
+  const annee = date.getFullYear();
+  const mois = String(date.getMonth() + 1).padStart(2, "0"); // Ajout du zéro devant si nécessaire
+  const jour = String(date.getDate()).padStart(2, "0"); // Ajout du zéro devant si nécessaire
+  const heure = String(date.getHours()).padStart(2, "0"); // Ajout du zéro devant si nécessaire
+  const minute = String(date.getMinutes()).padStart(2, "0"); // Ajout du zéro devant si nécessaire
+  const seconde = String(date.getSeconds()).padStart(2, "0");
+
+  // Création de la chaîne de caractères formatée
+  const sortie = `[${annee} ${mois} ${jour} - ${heure}:${minute}:${seconde}]`
+    .green;
+  const message = `Server started`.red;
+  const msg = `${sortie} ${message}`;
+  console.log(msg);
+
+  await loadFiles(path);
+  console.log("\n");
+};
+
+const loadFiles = async (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(path, (err, files) => {
+      if (err) {
+        console.log("Erreur lors de la lecture du répertoire :", err);
+        return;
+      }
+
+      files.forEach((file) => {
+        fs.stat(`${path}/${file}`, (err, stats) => {
+          if (err) {
+            console.error(
+              "Erreur lors de la récupération des informations sur le fichier :",
+              err
+            );
+            return;
+          }
+
+          if (stats.isDirectory()) {
+            loadFiles(`${path}/${file}`);
+          } else {
+            const date = new Date();
+
+            // Extraction des composants de la date
+            const annee = date.getFullYear();
+            const mois = String(date.getMonth() + 1).padStart(2, "0"); // Ajout du zéro devant si nécessaire
+            const jour = String(date.getDate()).padStart(2, "0"); // Ajout du zéro devant si nécessaire
+            const heure = String(date.getHours()).padStart(2, "0"); // Ajout du zéro devant si nécessaire
+            const minute = String(date.getMinutes()).padStart(2, "0"); // Ajout du zéro devant si nécessaire
+            const seconde = String(date.getSeconds()).padStart(2, "0");
+
+            // Création de la chaîne de caractères formatée
+            const sortie =
+              `[${annee} ${mois} ${jour} - ${heure}:${minute}:${seconde}]`
+                .green;
+            const message = `${path}/${file} loaded with success`.red;
+            const msg = `${sortie} ${message}`;
+            console.log(msg);
+          }
+        });
+      });
+    });
+
+    setTimeout(resolve, 1 * 1000);
+  });
+};
+
+async function load() {
   console.clear();
+  await loading("./server");
 
   console.log(
     `/*

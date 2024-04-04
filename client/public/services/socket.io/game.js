f811c57ea40ee6f9fe958b609fb8214124a07429
Enzoo_Kms <ekemmas@edenschool.fr>
2024-04-04 16:28:56 +0200

    refactor all

--- client/public/services/socket.io/game.js
+++ client/public/services/socket.io/game.js
@@ -1,33 +1,27 @@
-import Game from "../chess/html.js";
-
 const socket = io("/game");
-const dataGame = JSON.parse(
-  document.querySelector("input[data-game]").getAttribute("data-game")
-);
+const gameId = document.querySelector("input[name=gameId]").value;
 
 function set(game) {
   (Game.Game = game.board), (Game.currentPlayer = game.currentPlayer);
   Game.render();
 }
 
-socket.emit("join-room", dataGame.gameId);
+socket.emit("join-room", gameId);
 
 socket.on("join-room", (game) => {
   console.log(game);
   set(game);
 });
 
-socket.on("game-start", (isStart) => {
-  if (isStart) Game.start();
-});
+// socket.on("game-start", (isStart) => {
+//   if (isStart) Game.start();
+// });
 
-const playMove = (current, final) => {
-  socket.emit("playMove", dataGame.gameId, current, final);
-};
+// const playMove = (current, final) => {
+//   socket.emit("playMove", dataGame.gameId, current, final);
+// };
 
-socket.on("update-state", (game) => {
-  console.log(game);
-  set(game);
-});
-
-export { playMove };
+// socket.on("update-state", (game) => {
+//   console.log(game);
+//   set(game);
+// });
